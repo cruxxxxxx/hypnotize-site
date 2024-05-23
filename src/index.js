25ba2c660b88855288a3be97bfe1bec7a01c7c2a
crux <>
2024-05-23 15:58:27 -0400

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,108 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import SiteData from './sitedata.json';
+import useMouseEvents from './mouseEvents'
+
+const projectData = SiteData['projects'];
+
+gsap.registerPlugin(useGSAP);
+
+function Project(props) {
+  const project = props.project;
+  const isActive = props.isActive;
+  const onClick = props.onClick;
+
+  const outerProject = useRef();
+  const innerProject = useRef();
+  const projectInfo = useRef();
+
+  const { contextSafe } = useGSAP({ scope: outerProject });
+
+  const [countdown, setCountdown] = useState(5);
+
+  const { onMouseDown, onMouseUp, onMouseLeave, onMouseMove, cursorCircleRef } = useMouseEvents(onClick);
+
+  useEffect(() => {
+    if (isActive) {
+      const marginTop = project.marginTopOpen;
+      const marginBottom = project.marginBottomOpen;
+
+      gsap.to(projectInfo.current, { opacity: '100%' });
+
+      gsap.to(innerProject.current, { marginTop: marginTop });
+      gsap.to(innerProject.current, {
+        marginBottom: marginBottom,
+        onComplete: () => innerProject.current.scrollIntoView({ behavior: 'smooth', block: 'center' }),
+      });
+    } else {
+      const marginTop = project.marginTopClose;
+      const marginBottom = project.marginBottomClose;
+
+      gsap.to(projectInfo.current, { opacity: '0%' });
+
+      gsap.to(innerProject.current, { marginTop: marginTop });
+      gsap.to(innerProject.current, { marginBottom: marginBottom });
+    }
+  });
+
+  return (
+    <div
+      className="outer-project"
+      ref={outerProject}
+      onMouseDown={onMouseDown}
+      onMouseUp={onMouseUp}
+      onMouseLeave={onMouseLeave}
+      onMouseMove={onMouseMove}
+    >
+      <div className="inner-project" ref={innerProject}>
+        <img src={project.imgSrc} alt={project.name} />
+        <br />
+        <div className="project-info" ref={projectInfo}>
+          <div className="project-info-text">
+            title: {project.name} <br />
+            category: {project.category} <br />
+            date: {project.endDate} <br />
+            description: {project.description} <br />
+            for fans of: {project.forFansOf} <br />
+              <a href="http://www.google.com">fuckyea</a>
+          </div>
+        </div>
+      </div>
+      <div ref={cursorCircleRef} className="cursor-circle"></div>
+    </div>
+  );
+}
+
+function App() {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onClick = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  return (
+    <React.StrictMode>
+      <div id="header">hypnotize inc.</div>
+      <div id="main">
+        <div className="row">
+          <div className="column">
+            {projectData.map((project, index) => (
+              <Project
+                key={project.name}
+                project={project}
+                isActive={activeIndex === index}
+                onClick={() => onClick(index)}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </React.StrictMode>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
---
+++ src/mouseEvents.js
@@ -0,0 +1,73 @@
+import { useRef } from 'react';
+
+const useMouseEvents = (onClick) => {
+  const holdTimeoutRef = useRef(null);
+  const cursorCircleRef = useRef(null);
+  const mouseDragging = useRef(null);
+
+  const onMouseDown = (e) => {
+    console.log('mouse down!');
+
+    holdTimeoutRef.current = setTimeout(() => {
+
+      console.log('mouse held down');
+      cursorCircleRef.current.style.display = 'block';
+
+      updateCursorPosition(e);
+
+      holdTimeoutRef.current = setTimeout(() => { onClick() }, 2000);
+    }, 300);
+
+  };
+
+  const onMouseUp = () => {
+    console.log('mouse up');
+    clearTimeout(holdTimeoutRef.current);
+
+    cursorCircleRef.current.style.display = 'none';
+  };
+
+  const onMouseLeave = () => {
+    console.log('mouse leave');
+    clearTimeout(holdTimeoutRef.current);
+
+    cursorCircleRef.current.style.display = 'none';
+  };
+
+  const onMouseMove = (e) => {
+    if (mouseDragging.current) {
+      const currentX = e.clientX;
+      const currentY = e.clientY;
+
+      const dx = currentX - mouseDragging.current.startX;
+      const dy = currentY - mouseDragging.current.startY;
+
+      const change = Math.sqrt(dx * dx + dy * dy) ;
+      console.log('change ' + change);
+      if (change > 0.1) {
+        clearTimeout(holdTimeoutRef.current);
+        cursorCircleRef.current.style.display = 'none';
+        mouseDragging.current = null;
+      }
+
+    } else {
+      mouseDragging.current = { startX: e.clientX, startY: e.clientY };
+    }
+  };
+
+  const updateCursorPosition = (e) => {
+    const cursorCircle = cursorCircleRef.current;
+    cursorCircle.style.left = `${e.pageX - cursorCircle.offsetWidth / 2}px`;
+    cursorCircle.style.top = `${e.pageY - cursorCircle.offsetHeight / 2}px`;
+  };
+
+  return {
+    onMouseDown,
+    onMouseUp,
+    onMouseLeave,
+    onMouseMove,
+    cursorCircleRef
+  };
+};
+
+export default useMouseEvents;
